b020faa122ce53d082866434650f45e7feef15da
Ken Ha <ken@paymentwall.com>
2025-07-08 15:03:27 +0700

    Update Constants

--- app/src/main/java/com/terminal3/t3gamepaysdksample/config/Constants.kt
+++ app/src/main/java/com/terminal3/t3gamepaysdksample/config/Constants.kt
@@ -2,18 +2,15 @@ package com.terminal3.t3gamepaysdksample.config
 
 object Constants {
     // Endpoint on the merchant's server that make a charge requests to Brick API.
-//    const val MERCHANT_CHARGE_ENDPOINT = "https://merchant-server.com/api/charge"
-    const val MERCHANT_CHARGE_ENDPOINT = "https://api.paymentwall.com/api/brick/charge"
+    const val MERCHANT_CHARGE_ENDPOINT = "https://merchant-server.com/api/charge"
 
     // Secure URL to which the 3DS widget will redirect after successful authentication.
     // After redirection, the merchant should call MERCHANT_CHARGE_ENDPOINT again with the `token` to finalize the charge.
-    const val THREE_DS_RETURN_URL = "http://192.168.12.219:3000"
+    const val THREE_DS_RETURN_URL = "https://merchant-server.com/return-url"
 
     // production test
-    const val PW_PROJECT_KEY = "c741ef19877dd2047956cade07c9c7f6"; // GPay + Brick test
-    const val PW_SECRET_KEY = "28b8049b4d7d8292a2cc4d37d7a0ea81";
-//    const val PW_PROJECT_KEY = "";
-//    const val PW_SECRET_KEY = ""; // Should be get from merchant's server
+    const val PW_PROJECT_KEY = "";
+    const val PW_SECRET_KEY = ""; // Should be get from merchant's server
 
     const val USER_ID: String = "12311111001233123"
     const val ITEM_GEM_ID: String = "gem0001"
